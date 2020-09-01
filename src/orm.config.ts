a45a04c835b7da38792bf70d9ea80e8c80e6ca2a
Ilham <ilham.pirm@gmail.com>
2020-09-01 19:43:16 +0400

    Slide API

--- src/orm.config.ts
+++ src/orm.config.ts
@@ -2,12 +2,25 @@ import {TypeOrmModuleOptions} from "@nestjs/typeorm";
 
 export const config: TypeOrmModuleOptions = {
 
+    // type: "postgres",
+    // host: "localhost",
+    // port: 5432,
+    // username: 'postgres',
+    // password: "root",
+    // database: "tviser",
+    // entities: ["dist/**/*.entity.js"],
+    // synchronize: true,
+    // logging: true
+
+
+
+
     type: "postgres",
-    host: "localhost",
+    host: "c2-52-31-233-101.eu-west-1.compute.amazonaws.com",
     port: 5432,
-    username: 'postgres',
-    password: "root",
-    database: "tviser",
+    username: 'ivmuqrqxxukqkw',
+    password: "962336f9063cd90498b82c1c3711c35ba6a818143bf5d2a150d68e0b71ca1652",
+    database: "dcfmg399n4enmu",
     entities: ["dist/**/*.entity.js"],
     synchronize: true,
     logging: true
