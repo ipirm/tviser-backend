5dae8daf1b2605e8ff45b39f85490e8daba4554b
Ilham <ilham.pirm@gmail.com>
2020-09-01 20:09:27 +0400

    Slide API

--- src/orm.config.ts
+++ src/orm.config.ts
@@ -17,6 +17,7 @@ export const config: TypeOrmModuleOptions = {
 
     type: 'postgres',
     host: 'c2-52-31-233-101.eu-west-1.compute.amazonaws.com',
+    url: 'postgres://ivmuqrqxxukqkw:962336f9063cd90498b82c1c3711c35ba6a818143bf5d2a150d68e0b71ca1652@ec2-52-31-233-101.eu-west-1.compute.amazonaws.com:5432/dcfmg399n4enmu',
     port: 5432,
     username: 'ivmuqrqxxukqkw',
     password: '962336f9063cd90498b82c1c3711c35ba6a818143bf5d2a150d68e0b71ca1652',
