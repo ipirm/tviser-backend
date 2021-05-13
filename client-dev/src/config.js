e16bc23485000f5d4402c7e151c8505346c6d2a9
unknown <ilham>
2021-05-13 09:47:41 +0400

    New User

--- client-dev/src/config.js
+++ client-dev/src/config.js
@@ -1,2 +1,2 @@
-export const API_URI = 'https://tviser-backend.herokuapp.com/api';
+export const API_URI = 'https://tviser-backend2.herokuapp.com/api';
 // export const API_URI = 'http://localhost:3000/api';
